b6a3f92e153b405b238a7853a8c04c3d5a611509
Ali Hejazi <seyed.ali.hejazi.35@gmail.com>
2020-04-10 21:51:19 +0430

    initial commit

--- target/classes/static/js/registration.js
+++
@@ -1,9 +0,0 @@
-function registrationFormSubmit() {
-    let rulesCheckBoxElm = document.getElementById('rulesCheckBox');
-    if (!rulesCheckBoxElm.checked){
-        alert('you should agree to the rules.')
-    } else {
-        let form = document.getElementsByTagName('form');
-        form[0].submit();
-    }
-}
